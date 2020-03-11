d8eded32be76beb11b28fc3d0f890ad2c17e70ba
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-11 23:41:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,18 @@ class Student
     @name = name 
     @grade = grade 
     @id = id
+  end 
+  
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
  
   
 end
